fdacd486aa0f31c33e3f8098d3532c50f49c230f
LoganElmore <logan.elmore@knights.ucf.edu>
2023-04-24 13:57:51 -0400

    Implementing login and sign up buttons

--- EcoTrack/screens/SignUpLoginScreen.js
+++ EcoTrack/screens/SignUpLoginScreen.js
@@ -1,8 +1,32 @@
 import React, { useState } from 'react';
-import {
-  StyleSheet,
-  Text,
-  View,
-  TextInput,
-  TouchableOpacity,
-} from 'react-native';
+import { StyleSheet, Text, View, TextInput, TouchableOpacity, Touchable } from 'react-native';
+
+const SignUpLoginScreen = ({navigation}) => {
+    return (
+        <View style={styles.container}>
+            <Text style={styles.title}> Welcome to EcoTrack </Text>
+            <TextInput
+                style={styles.input}
+                placeholder="Email"
+                keyboardType="email-address"
+                autoCapitalize="none"
+            />
+            <TextInput
+                style={styles.input}
+                placeholder="Password"
+                secureTextEntry
+            />
+            <View style={styles.buttonContainer}>
+                <TouchableOpacity
+                    style={styles.signUpButton}
+                    onPress={() => navigation.navigate('SignUp')}
+                </TouchableOpacity>
+                <TouchableOpacity
+                    style={styles.loginButton}
+                    onPress={() => navigation.navigate('Home')}
+                </TouchableOpacity>
+            
+            </View>
+        </View>
+    )
+}
