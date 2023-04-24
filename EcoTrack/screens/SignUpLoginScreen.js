e2ec689df52f10a600444a9c0ebeefcb7e18a248
LoganElmore <logan.elmore@knights.ucf.edu>
2023-04-24 14:20:07 -0400

    Styling + various fixes

--- EcoTrack/screens/SignUpLoginScreen.js
+++ EcoTrack/screens/SignUpLoginScreen.js
@@ -1,50 +1,96 @@
-import React, { useState } from 'react';
-import { StyleSheet, Text, View, TextInput, TouchableOpacity, Touchable } from 'react-native';
+import React from 'react';
+import { StyleSheet, Text, View, TextInput, TouchableOpacity } from 'react-native';
 
-const SignUpLoginScreen = ({navigation}) => {
-    // Rendering the SignUpLoginScreen component
-    return (
-        <View style={styles.container}>
-            {/* Display the welcome title */}
-            <Text style={styles.title}> Welcome to EcoTrack </Text>
-
-            {/* Email input field */}
-            <TextInput
-                style={styles.input}
-                placeholder="Email"
-                keyboardType="email-address"
-                autoCapitalize="none"
-            />
-
-            {/* Password input field */}
-            <TextInput
-                style={styles.input}
-                placeholder="Password"
-                secureTextEntry
-            />
-
-            {/* Container for Sign Up and Login buttons */}
-            <View style={styles.buttonContainer}>
-
-            {/* Sign Up button, navigates to SignUp screen */}
-            <TouchableOpacity
-                style={styles.signUpButton}
-                onPress={() => navigation.navigate('SignUp')}>
-            
-            <Text style={styles.buttonText}>Sign Up</Text>
-            </TouchableOpacity>
-            {/* Login button, navigates to Home screen */}
-
-                <TouchableOpacity
-                    style={styles.loginButton}
-                    onPress={() => navigation.navigate('Home')}>
-                </TouchableOpacity>
-
-            <Text style={styles.buttonText}>Login</Text>
-            </TouchableOpacity>
-            
-        </View>
-        </View>
-    );
-
+const SignUpLoginScreen = ({ navigation }) => {
+  // Render the SignUpLoginScreen component
+  return (
+    <View style={styles.container}>
+      {/* Display the welcome title */}
+      <Text style={styles.title}>Welcome to EcoTrack</Text>
+      
+      {/* Email input field */}
+      <TextInput
+        style={styles.input}
+        placeholder="Email"
+        keyboardType="email-address"
+        autoCapitalize="none"
+      />
+      
+      {/* Password input field */}
+      <TextInput
+        style={styles.input}
+        placeholder="Password"
+        secureTextEntry
+      />
+      
+      {/* Container for Sign Up and Login buttons */}
+      <View style={styles.buttonContainer}>
+        {/* Sign Up button, navigates to SignUp screen */}
+        <TouchableOpacity
+          style={styles.signUpButton}
+          onPress={() => navigation.navigate('SignUp')}
+        >
+          <Text style={styles.buttonText}>Sign Up</Text>
+        </TouchableOpacity>
+        
+        {/* Login button, navigates to Home screen */}
+        <TouchableOpacity
+          style={styles.loginButton}
+          onPress={() => navigation.navigate('Home')}
+        >
+          <Text style={styles.buttonText}>Login</Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
 };
+
+// Styling for Signup/Login Component
+const styles= StyleSheet.create({
+    container: {
+        flex:1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: '#F0F8FF',
+    },
+title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    color: '#2E8B57',
+    marginBottom: 20,
+},
+input: {
+    width: '80%',
+    borderWidth: 1,
+    borderColor: '#999',
+    borderRadius: 5,
+    paddingHorizontal: 15,
+    paddingVertical: 10,
+    fontSize: 16,
+    marginBottom: 20,
+},
+buttonContainer: {
+    flexDirection: 'row',
+},
+signUpButton: {
+    backgroundColor: '#3CB371',
+    paddingHorizontal: 30,
+    paddingVertical: 15,
+    borderRadius: 10,
+    marginRight: 10,
+},
+loginButton: {
+    backgroundColor: '#3CB371',
+    paddingHorizontal: 30,
+    paddingVertical: 15,
+    borderRadius: 10,
+    marginLeft: 10,
+},
+buttonText: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    color: '#FFF',
+},
+});
+
+export default SignUpLoginScreen;
