b2b0d59174988af34f7e670dd55e78972fb525bc
LoganElmore <logan.elmore@knights.ucf.edu>
2023-04-24 14:24:54 -0400

    Importing SignUpLoginScreen to App.js, create 2 more screens

--- EcoTrack/App.js
+++ EcoTrack/App.js
@@ -3,23 +3,42 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
 // OnboardingScreen component import
+// Import the required screen components
 import OnboardingScreen from './screens/OnboardingScreen';
-// Signup Screen component import
 import SignUpLoginScreen from './screens/SignUpLoginScreen';
+import SignUpScreen from './screens/SignUp'; // Import the SignUpScreen component
+import HomeScreen from './screens/Home'; // Import the HomeScreen component
 
 const Stack = createStackNavigator();
 
 const App = () => {
-    return (
-        <NavigationContainer>
-            <Stack.Navigator initialRouteName="Onboarding">
-                <Stack.Screen
-                    name="Onboarding"
-                    component={OnboardingScreen}
-                    options={{headerShown: false}}
-                    />
-        {/*Adding more screens as I go*/}
-        </Stack.Navigator>
-        </NavigationContainer>
-    );
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Onboarding">
+        <Stack.Screen
+          name="Onboarding"
+          component={OnboardingScreen}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="SignUpLogin"
+          component={SignUpLoginScreen}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="SignUp"
+          component={SignUp}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="Home"
+          component={Home}
+          options={{ headerShown: false }}
+        />
+        {/* Add more screens as needed */}
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 };
+
+export default App;
