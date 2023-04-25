6cbaaae2bfb0e957e9f4cdc8b3c644a097383567
LoganElmore <logan.elmore@knights.ucf.edu>
2023-04-24 20:43:35 -0400

    ProfileHome creation

---
+++ Ecotrack/screens/ProfileHome.js
@@ -0,0 +1,13 @@
+import React, {useState, useEffect} from 'react';
+import { View, Text, Image, StyleSheet } from 'react-native';
+
+const ProfileHome = () => {
+    cont [Profile, setProfile] = useState({
+        name:'',
+        photo: null,
+        transportation:'',
+        vehicleType:'',
+        heatingSource:'',
+        numberOfPeople:'',
+    });
+}
