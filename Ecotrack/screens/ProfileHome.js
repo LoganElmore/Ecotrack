97df8066fd501625e193ef95a69096ab9bb3a622
LoganElmore <logan.elmore@knights.ucf.edu>
2023-04-24 20:54:38 -0400

    Profile import functionality complete

--- Ecotrack/screens/ProfileHome.js
+++ Ecotrack/screens/ProfileHome.js
@@ -10,4 +10,40 @@ const ProfileHome = () => {
         heatingSource:'',
         numberOfPeople:'',
     });
-}
+
+    // Data simulating input from Profile.js form
+    useEffect(() => {
+        setProfile({
+            name: 'Little Bubs',
+            photo: null,
+            transportation: 'Car',
+            vehicleType: 'Sedan',
+            heatingSource: 'Electric',
+            numberOfPeople: 4,
+        });
+    }, []);
+
+    return (
+        <View style={styles.container}>
+            <Text style={styles.title}>Your Profile</Text>
+
+            {Profile.photo ? (
+                <Image source={{uri: Profile.photo }} style={styles.profileImage}/>
+            ) : (
+                <View style={styles.profileImagePlaceholder}>
+                <Text style={styles.placeholderText}>No Photo Added</Text>
+                </View>
+            )}
+
+        <Text>Name: {profile.name}</Text>
+
+        <Text>Transportation: {profile.transportation}</Text>
+        {profile.transportation === 'Car' && (
+        <Text>Vehicle Type: {profile.vehicleType}</Text>
+        )}
+        
+        <Text>Heating Source: {profile.heatingSource}</Text>
+        <Text>Number of People: {profile.numberOfPeople}</Text>
+        </View>
+        );
+};
